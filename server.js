674c53eb43865adb8111bd9b3f94c41b43d0535b
nikhil051097 <nikhil051097@gmail.com>
2017-04-17 11:43:02 +0530

    change in environment port

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var todos = require('./routes/todos');
 
+const port=process.env.PORT || 3000;
+
 var app = express();
 
 // View Engine
@@ -20,8 +22,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use('/', index);
 app.use('/api/v1/', todos);
 
-app.listen(3000, function(){
-    console.log('Server started on port 3000...');
+app.listen(port, function(){
+    console.log(`Server started on port 3000... ${port}`);
 });
 
 
